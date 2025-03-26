fe06cd102827cedeedbc03b9d4a2d27616310f68
Kiera Reidy <keelouise@yahoo.com>
2025-03-26 10:15:54 +0000

    Updated createaccount.JS
    Initial JS code for createaccount.html script. KR 26/03/2025

--- static/javascript/createaccount.js
+++ static/javascript/createaccount.js
@@ -1,2 +1,17 @@
+document.getElementById("createAccount").addEventListener("submit", function(event) {
+    // This will clear any previous error messages - KR 26/03/2025
+    const emailError = document.getElementById("emailError");
+    const passwordError = document.getElementById("passwordError");
+    const confirmPasswordError = document.getElementById("confirmPasswordError");
+    emailError.textContent = "";
+    passwordError.textContent = "";
+    confirmPasswordError.textContent = "";
 
+    // This will retrieve form values - KR 26/03/2025
+    var fullName = document.getElementById("fullname").value;
+    var email = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("exampleInputPassword1").value;
+    var confirmPassword = document.getElementById("exampleInputPassword2").value;
+
+    var valid = true;
 
