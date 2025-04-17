50657ed5f1f283c9d5a0dfe2bced5938fb6f8e8f
Kiera Reidy <keelouise@yahoo.com>
2025-04-17 13:13:46 +0100

    Updated for 3rd party cookie handling. KR 17/04/2025

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,12 @@ def create_app():
     # Secret key for sessions and flash messages - KR 28/03/2025
     app.config['SECRET_KEY'] = 'NEBULA_2025'
 
+    app.config.update(
+    SESSION_COOKIE_SECURE=True,      # Cookies to be sent over HTTPS only - KR 17/04/2025
+    SESSION_COOKIE_HTTPONLY=True,    # Prevent JavaScript accessing cookies - KR 17/04/2025
+    SESSION_COOKIE_SAMESITE='Lax'    # Added after further research as this helps protect against CSRF - KR 17/04/2025
+)
+
     register_routes(app)
 
     return app
